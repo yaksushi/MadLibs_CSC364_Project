e260e17f4747740e6d7cd8c051a50678b1c64310
Mark Watanabe <yaksushi@users.noreply.github.com>
2016-11-29 12:38:42 -0500

    Updated meta and logout
    updated main image and fixed logout.php
    added inc_headerScripts.php and inc_meta.php

---
+++ inc_meta.php
@@ -0,0 +1,17 @@
+<!-- Include file for meta information -->
+
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="Mad Libs CSC322 Final Project - Login" content="" />
+<meta name="Mark Watanabe" content="" />
+<link rel="icon" href="assets/favicon.ico" />
+<!-- References and Notes -->
+<!-- Image sources:	http://www.madlibs.com/books/
+                    http://www.adamzyglis.com/
+                    http://www.original-political-cartoon.com/cartoon-gallery/artists/mahony-francis-william-1905-1982/
+ -->
+<!-- Help sources: https://www.lynda.com/MySQL-tutorials/PHP-MySQL-Essential-Training/119003-2.html
+                  http://www.codingcage.com/2015/03/simple-login-and-signup-system-with-php.html
+-->
